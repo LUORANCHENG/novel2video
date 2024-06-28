62dad6b41a88ce73edc3775e5912733259a9e041
LLL <80999506+LUORANCHENG@users.noreply.github.com>
2024-06-28 21:45:18 +0800

    Update Chattts_test.py

--- test/Chattts_test.py
+++ test/Chattts_test.py
@@ -2,4 +2,4 @@ import utils.tts_pro as tts
 
 text = "叶婉晴从不知道一天一夜有那么漫长，漫长到可以让一个人从希望到绝望，从鲜活到行尸走肉，连心脏都被冻结。"
 tts.generate_audio(text=text, language='cn', txt_name="test", order=1)
-print(f"文件保存在./output/audio/test/cn/1.mp3")
+print(f"文件保存在./test/output/audio/test/cn/1.mp3")
